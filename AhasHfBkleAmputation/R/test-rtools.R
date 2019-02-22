c233629bb8cde1d738440d27b24d947847236400
Sigfried Gold <sigfried.gold@nih.gov>
2019-02-22 13:45:35 -0500

    nothing much

--- AhasHfBkleAmputation/R/test-rtools.R
+++ AhasHfBkleAmputation/R/test-rtools.R
@@ -1,7 +1,8 @@
+#library(tidyverse)
+readRenviron('.env')
 
 library(DatabaseConnector)
-library(tidyverse)
-readRenviron('.env')
+library(SqlRender)
 
 connectionDetails <- createConnectionDetails( dbms="postgresql", 
                                               server=paste0(Sys.getenv("PGHOST"),'/',
@@ -9,12 +10,10 @@ connectionDetails <- createConnectionDetails( dbms="postgresql",
                                               user = Sys.getenv('PGUSER'),
                                               password = Sys.getenv('PGPASSWORD'),
                                               schema="mimic3_100p")
-
 conn <- connect(connectionDetails)
 
-
-
-
+# getwd()
+# [1] "/export/home/goldss/projects/StudyProtocols/AhasHfBkleAmputation"
 
 OhdsiRTools::insertCohortDefinitionSetInPackage(fileName = "settings.csv",
                                                 baseUrl = "http://api.ohdsi.org:80/WebAPI",
@@ -22,3 +21,37 @@ OhdsiRTools::insertCohortDefinitionSetInPackage(fileName = "settings.csv",
                                                 insertCohortCreationR = TRUE,
                                                 generateStats = FALSE,
                                                 packageName="ischemicStrokePhenotype")
+# Inserting cohort: Male50plus
+# Error in readChar(fileName, file.info(fileName)$size) : 
+#   invalid 'nchars' argument
+# In addition: Warning message:
+#   In file(con, "rb") :
+#   file("") only supports open = "w+" and open = "w+b": using the former
+
+#not working
+renderedSql <- loadRenderTranslateSql("Male50plus.sql",
+                                      #packageName = "CohortMethod", # tried this and below
+                                      packageName = "ischemicStrokePhenotype",
+                                      dbms = connectionDetails$dbms,
+                                      CDM_schema = "mimic3_100p_results")
+
+
+
+stmnt = "select top 10 * from person"
+
+#SqlRender::translateSql(stmnt, targetDialect = 'postgresql')
+
+
+# Error in readChar(pathToSql, file.info(pathToSql)$size) : 
+#   invalid 'nchars' argument
+# In addition: Warning message:
+#   In file(con, "rb") :
+#   file("") only supports open = "w+" and open = "w+b": using the former
+
+# same error with many other variations:
+# SqlRender::loadRenderTranslateSql('./inst/sql/sql_server/Male50plus.sql', 'ischemicStrokePhenotype', 'postgres')
+# SqlRender::loadRenderTranslateSql('./inst/sql/sql_server/Male50plus.sql', packageName = 'ischemicStrokePhenotype')
+# SqlRender::loadRenderTranslateSql('Male50plus.sql', 'ischemicStrokePhenotype', 'sql server')
+# SqlRender::loadRenderTranslateSql('Male50plus.sql', 'ischemicStrokePhenotype', 'sql_server')
+# SqlRender::loadRenderTranslateSql('Male50plus.sql', 'ischemicStrokePhenotype', 'sqlserver')
+# SqlRender::loadRenderTranslateSql('Male50plus.sql', 'ischemicStrokePhenotype')
